ec34f27299c727cb458ac35f4f4ed91279bdcdae
ara818 <ara818@gmail.com>
2011-03-16 11:38:08 -0400

    Add initial setup files.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-autocompleter',
+    version="0.1.0",
+    description='A redis-backed autocompletor for Django projects',
+    author='Ara Anjargolian',
+    author_email='ara818@gmail.com',
+    url='http://github.com/ara818/django-autocompleter',
+    packages=find_packages('autocompleter'),
+    install_requires = ['setuptools'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
