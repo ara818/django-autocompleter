aea79f37b1d26db1a49d2653bdaf2fcd381d81e2
ara818 <ara818@gmail.com>
2011-04-13 23:53:24 -0400

    Add back autodiscover, but based on models.py. Turns out it comes in handy for management command

--- autocompleter/__init__.py
+++ autocompleter/__init__.py
@@ -2,3 +2,35 @@ VERSION = (0, 1, 1)
 
 from autocompleter.registry import registry
 from autocompleter.base import AutocompleterProvider, Autocompleter
+
+LOADING_AUTOCOMPELTER = False
+def autodiscover():
+    """
+    Auto-discover INSTALLED_APPS autocompleters.py modules and fail silently when
+    not present.
+    NOTE: autodiscover was copied from django.contrib.admin autodiscover
+    """
+    global LOADING_AUTOCOMPELTER
+    if LOADING_AUTOCOMPELTER:
+        return
+    LOADING_AUTOCOMPELTER = True
+
+    import imp
+    from django.utils.importlib import import_module
+    from django.conf import settings
+
+    for app in settings.INSTALLED_APPS:
+
+        try:
+            app_path = import_module(app).__path__
+        except AttributeError:
+            continue
+
+        try:
+            imp.find_module('models', app_path)
+        except ImportError:
+            continue
+
+        import_module("%s.autocompleters" % app)
+
+    LOADING_AUTOCOMPELTER = False
