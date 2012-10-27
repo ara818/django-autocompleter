98f273784cc34979bf7cb00c26d8a63a48aaa374
ara818 <ara818@gmail.com>
2012-10-27 15:52:44 -0700

    rename views

--- autocompleter/urls.py
+++ autocompleter/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('autocompleter.views',
-    url(r'^suggest/(?P<name>[0-9A-Za-z_-]+)$', 'suggest', name='suggest_named'),
-    url(r'^exact_suggest/(?P<name>[0-9A-Za-z_-]+)$', 'exact_suggest', name='exact_suggest_named'),
+    url(r'^suggest/(?P<name>[0-9A-Za-z_-]+)$', 'suggest', name='suggest'),
+    url(r'^exact_suggest/(?P<name>[0-9A-Za-z_-]+)$', 'exact_suggest', name='exact_suggest'),
 )
