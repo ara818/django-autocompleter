859678365d5ad19702c1d083498028a18344b17b
James Bianco <jsbianco33@gmail.com>
2013-07-29 18:33:05 -0400

    dict provider set up.  just tweaking naming/tests
    removed egg junk from previous commit

--- autocompleter/registry.py
+++ autocompleter/registry.py
@@ -175,7 +175,7 @@ class AutocompleterSignalRegistry(object):
         post_save.connect(add_obj_to_autocompleter, sender=model,
             dispatch_uid='autocompleter.%s.add' % (model))
         pre_save.connect(remove_old_obj_from_autocompleter, sender=model,
-            dispatch_uid='autocompleter.%s.remoe_old' % (model))
+            dispatch_uid='autocompleter.%s.remove_old' % (model))
         post_delete.connect(remove_obj_from_autocompleter,
             sender=model, dispatch_uid='autocompleter.%s.remove' % (model))
 
@@ -183,7 +183,7 @@ class AutocompleterSignalRegistry(object):
         post_save.disconnect(add_obj_to_autocompleter,
             sender=model, dispatch_uid='autocompleter.%s.add' % (model))
         pre_save.disconnect(remove_old_obj_from_autocompleter, sender=model,
-            dispatch_uid='autocompleter.%s.remoe_old' % (model))
+            dispatch_uid='autocompleter.%s.remove_old' % (model))
         post_delete.disconnect(remove_obj_from_autocompleter,
             sender=model, dispatch_uid='autocompleter.%s.remove' % (model))
 
