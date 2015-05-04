dddd04cd6605eec896c876fdc2d6088c9e0efd0d
Ryan Kuczka <ryan.kuczka@gmail.com>
2015-05-04 18:18:18 -0400

    Update autodiscover and add apps.py for Django 1.7
    Also, no longer officially support < Django 1.7 although I don't see any
    reason it wouldn't work. It just would be able to rely on autodiscover()

---
+++ autocompleter/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+
+
+class SimpleAutocompleterConfig(AppConfig):
+    """ Simple AppConfig which does not do automatic discovery. """
+    name = 'autocompleter'
+    verbose_name = 'Autocompleter'
+
+
+class AutocompleterConfig(SimpleAutocompleterConfig):
+    """ The default AppConfig for autocompleter which does autodiscovery. """
+
+    def ready(self):
+        self.module.autodiscover()
