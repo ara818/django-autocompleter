7358ac668c77fb7d478ae94bdbb8f74e418d39ff
jakeway <tom@ycharts.com>
2018-06-28 11:42:25 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-autocompleter',
-    version="0.7.0",
+    version="0.7.1",
     description='A redis-backed autocompletor for Django projects',
     author='Ara Anjargolian',
     author_email='ara818@gmail.com',
