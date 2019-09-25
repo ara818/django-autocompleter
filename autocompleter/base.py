ca9881e24da522302c449f36be0a574d36c5332e
Ara Anjargolian <ara818@gmail.com>
2019-09-25 18:51:27 -0400

    forgot one thing to make it py-redis 2 compatible while making it easy to switch to py-redis 3

--- autocompleter/base.py
+++ autocompleter/base.py
@@ -305,7 +305,7 @@ class AutocompleterProviderBase(AutocompleterBase):
                     word_prefix += char
                     # Store prefix to obj ID mapping, with score
                     key = PREFIX_BASE_NAME % (provider_name, word_prefix,)
-                    pipe.zadd(key, {obj_id: score})
+                    pipe.zadd(key, **{obj_id: score})
                     # Store autocompleter to prefix mapping so we know all prefixes
                     # of an autocompleter
                     key = PREFIX_SET_BASE_NAME % (provider_name,)
@@ -320,7 +320,7 @@ class AutocompleterProviderBase(AutocompleterBase):
                     continue
                 # Store exact term to obj ID mapping, with score
                 key = EXACT_BASE_NAME % (provider_name, norm_term,)
-                pipe.zadd(key, {obj_id: score})
+                pipe.zadd(key, **{obj_id: score})
 
                 # Store autocompleter to exact term mapping so we know all exact terms
                 # of an autocompleter
@@ -329,7 +329,7 @@ class AutocompleterProviderBase(AutocompleterBase):
 
         for facet in facet_dicts:
             key = FACET_SET_BASE_NAME % (provider_name, facet['key'], facet['value'],)
-            pipe.zadd(key, {obj_id: score})
+            pipe.zadd(key, **{obj_id: score})
 
         # Map provider's obj_id -> data payload
         key = AUTO_BASE_NAME % (provider_name,)
