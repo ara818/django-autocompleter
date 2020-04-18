43c1dc429e535d045e8eb679fb90ccf96b46a735
Ara Anjargolian <ara818@gmail.com>
2020-04-17 21:11:42 -0700

    Improve base test case

--- test_project/test_app/tests/base.py
+++ test_project/test_app/tests/base.py
@@ -7,19 +7,25 @@ from django.test import TestCase
 
 class AutocompleterTestCase(TestCase):
     def setUp(self):
-        self.redis = redis.Redis(host=settings.AUTOCOMPLETER_REDIS_CONNECTION['host'],
+        self.redis = redis.Redis(
+            host=settings.AUTOCOMPLETER_REDIS_CONNECTION['host'],
             port=settings.AUTOCOMPLETER_REDIS_CONNECTION['port'],
             db=settings.AUTOCOMPLETER_REDIS_CONNECTION['db'])
 
-        # purge any possible old test data in case of previous failures where tearDown didn't fire.
-        # This is hardcoded so you don't accidentally wipe your redis db somehow.
+    def tearDown(self):
+        # Purge any possible old test data
         old_data = self.redis.keys("djac.test.*")
         pipe = self.redis.pipeline()
-        for i in old_data:
-            pipe.delete(i)
+        for i in self.chunk_list(old_data, 100):
+            pipe.delete(*i)
         pipe.execute()
 
     @classmethod
     def tearDownClass(cls):
         super(AutocompleterTestCase, cls).tearDownClass()
         management.call_command('flush', verbosity=0, interactive=False)
+
+    @staticmethod
+    def chunk_list(lst, chunk_size):
+        for i in range(0, len(lst), chunk_size):
+            yield lst[i:i + chunk_size]
