18365803edd7eba03d47033c0d5aea39f0fe5ede
Kevin Dabiedeen <kdabiedeen1@gmail.com>
2020-06-16 15:46:22 -0400

    update version to 0.10.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-autocompleter',
-    version="0.9.0",
+    version="0.10.0",
     description='A redis-backed autocompletor for Django projects',
     author='Ara Anjargolian',
     author_email='ara818@gmail.com',
